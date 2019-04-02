335b9500c89691f010e129d128c91425e78309d7
Alex <astrodeep@ya.ru>
2019-04-02 11:49:41 +0300

    fix

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,7 +1,3 @@
 import "owl.carousel";
 import "../modules/slider/slider";
-import { parseNumber } from 'libphonenumber-js';
-parseNumber('phoneNumber: 8 (800) 555 35 35.', 'RU');
-import { formatNumber } from 'libphonenumber-js'
-formatNumber('+12133734253', 'International');
 
